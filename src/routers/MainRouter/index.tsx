5f610ee7944e12531868e89efa244eb07c1a88ea
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2026-01-26 10:30:36 -0400

    Adicionando history e seu css

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -3,6 +3,7 @@ import { AboutPomodoro } from '../../pages/AboutPomodoro';
 import { Home } from '../../pages/Home';
 import { NotFound } from '../../pages/NotFound';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+import { History } from '../../pages/History';
 
 function ScrollToTop() {
     const { pathname } = useLocation();
@@ -18,6 +19,7 @@ export function MainRouter() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/history/' element={<History />} />
                 <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
 
                 <Route path='*' element={<NotFound />} />
